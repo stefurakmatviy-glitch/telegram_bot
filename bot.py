44d33074251f791e40525533af6591033029c15c
Matviy StefuraK <stefurakmatviy@gmail.com>
2025-12-14 02:23:28 -0500

    Перший коміт бота

---
+++ bot.py
@@ -0,0 +1,26 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8382200509:AAGALdtdIcruhdZLVbOcJFiJapGKJcy_pL4"
+
+async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("📜 Правила чату", url="https://pastebin.com/SaYSTsFW")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(
+        "Правила чату",
+        reply_markup=reply_markup
+    )
+
+async def shluhi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Контакти всіх шлюх Чикаго у Кірюші, звертайтеся до нього!"
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("rules", rules))
+app.add_handler(CommandHandler("shluhi", shluhi_command))
+
+print("Бот запущений. Чекаю команди /rules або /shluhi …")
+app.run_polling()
